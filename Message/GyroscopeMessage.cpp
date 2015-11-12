999b19ea34dea947d0e4d2300f4f78424dd67109
Dalmir Silva <dalmirdasilva@gmail.com>
2015-11-12 17:34:38 -0200

    sync

--- Message/GyroscopeMessage.cpp
+++ Message/GyroscopeMessage.cpp
@@ -2,7 +2,7 @@
 #include <Message.h>
 
 GyroscopeMessage::GyroscopeMessage()
-        : Message(NAVIGATION_PROTOCOL_GYROSCOPE_MESSAGE_FLAGS) {
+        : Message(GYROSCOPE_MESSAGE_TYPE) {
     payloadSize = (uint8_t) sizeof(_payload);
     flags = NAVIGATION_PROTOCOL_GYROSCOPE_MESSAGE_FLAGS;
     payload = (uint8_t *) &_payload;
